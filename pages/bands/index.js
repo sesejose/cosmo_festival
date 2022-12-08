6f4644eb27d70bf8158fd5cd3048f34670890061
Karina Valens <karyvalens@yahoo.com>
2022-12-08 09:52:53 +0100

    adding # to the bands endpoint

--- pages/bands/index.js
+++ pages/bands/index.js
@@ -24,7 +24,7 @@ export default function Bands({bands}) {
     - once we have the data, it put into the component
     - so the component can render with that data inside it  */
 
-    const res = await fetch("http://localhost:8080/bands");
+    const res = await fetch("http://localhost:8080/bands#");
     const data = await res.json();
 
 /* - we return a value for this function 
