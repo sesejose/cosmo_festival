fb4564ae9b897ba706665a46251f40316dd74750
Nikolett Dékány <niko703j@stud.kea.dk>
2022-12-12 10:33:59 +0100

    COSMO

--- components/Herosection.js
+++ components/Herosection.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import heroImg from "/public/festival-landing.jpg";
+
 export default function Herosection() {
   return (
     <section className={styles.herosection}>
       <Image src={heroImg} alt="festival" width={1512} height={830} />
-      <h1>Cosmo</h1>
+      <h1>COSMO</h1>
       <h2>15/12</h2>
       <div className={styles.callTA}>
         <h3>
