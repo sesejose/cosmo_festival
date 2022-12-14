6364350888410a1b7536abc2b62287b8030efa01
José <sesejose@gmail.com>
2022-12-14 18:12:09 +0100

    Passing Tickets Quantity to the Basket

--- components/Basket.js
+++ components/Basket.js
@@ -1,8 +1,44 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 function Basket(regTickets, vipTickets) {
+  const [data, setData] = useState();
   const quaReg = regTickets;
   const quaVip = vipTickets;
+
+  // Fetching the data
+  useEffect(() => {
+    async function getData() {
+      const url = "https://udfchraccrfladlsvbzh.supabase.co/rest/v1/cosmo_festival";
+      const headers = {
+        "Content-Type": "application/json",
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkZmNocmFjY3JmbGFkbHN2YnpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA4NzQzODEsImV4cCI6MTk4NjQ1MDM4MX0.0eTW-TRibvc-FFW6XlCaTEfX52g-3SsrjMh3t7XXvIw",
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkZmNocmFjY3JmbGFkbHN2YnpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA4NzQzODEsImV4cCI6MTk4NjQ1MDM4MX0.0eTW-TRibvc-FFW6XlCaTEfX52g-3SsrjMh3t7XXvIw",
+        Prefer: "return-representation",
+      };
+      const options = {
+        method: "GET",
+        headers: headers,
+      };
+      // Await then execute the code.
+      const res = await fetch(url, options); // Fetchs the data (await)
+      const data = await res.json(); //When it's done getting it
+      // return data; // This returned "data/array" used in the showData();
+      setData(data);
+      console.log(data);
+    }
+    getData();
+  }, []);
+
+  // init();
+
+  // async function init() {
+  //   showTickets();
+  // }
+
+  // function addTickets(data) {
+  // }
+
   showTickets();
 
   async function showTickets() {
