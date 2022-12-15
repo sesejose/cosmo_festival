4c7f322fd77f0c5f5be75c8d25de236307b6d8af
José <sesejose@gmail.com>
2022-12-15 11:31:00 +0100

    tickets component deploy again

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Herosection from "../components/Herosection";
 import Lineup from "../components/Lineup";
 import styles from "../styles/Home.module.css";
-import Tickets from "../components/TIckets";
+import Tickets from "../components/Tickets";
 import Accomodation from "../components/Accomodation";
 import Basket from "../components/Basket";
 
