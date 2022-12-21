84fdd3f5e2c770c435ea48996cca830d09b5629c
Nikolett Dékány <niko703j@stud.kea.dk>
2022-12-21 06:56:55 +0100

    changed link to live

--- pages/index.js
+++ pages/index.js
@@ -82,14 +82,14 @@ export default function Home({ areas, schedule, bands }) {
 }
 export async function getServerSideProps() {
   const [scheduleRes, bandsRes, areasRes] = await Promise.all([
-    // fetch(`https://bitter-moon-5524.fly.dev/schedule`),
-    fetch("http://localhost:8080/schedule"),
+    fetch(`https://bitter-moon-5524.fly.dev/schedule`),
+    //fetch("http://localhost:8080/schedule"),
 
-    // fetch(`https://bitter-moon-5524.fly.dev/bands`),
-    fetch("http://localhost:8080/bands"),
+    fetch(`https://bitter-moon-5524.fly.dev/bands`),
+    //fetch("http://localhost:8080/bands"),
 
-    // fetch(`https://bitter-moon-5524.fly.dev/available-spots`),
-    fetch("http://localhost:8080/available-spots"),
+    fetch(`https://bitter-moon-5524.fly.dev/available-spots`),
+    //fetch("http://localhost:8080/available-spots"),
   ]);
 
   const [schedule, bands, areas] = await Promise.all([scheduleRes.json(), bandsRes.json(), areasRes.json()]);
